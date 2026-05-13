da64b1f34150fe8e826b5a51d3060e6845c253a1
pmamathatechm <pmamatha160@gmail.com>
2026-05-13 21:11:26 +0530

    Update Cart

--- Cart.spec.ts
+++ Cart.spec.ts
@@ -1,1 +1,22 @@
+mport { test, expect } from '@playwright/test';
+import { CartPage } from '../pages/CartPage';
 
+test('Add products to cart and checkout', async ({ page }) => {
+
+  const cartPage = new CartPage(page);
+
+  // Launch application
+  await cartPage.launchApplication();
+
+  // Add shoes product
+  await cartPage.addShoesToCart();
+
+  // Add T-shirt product
+  await cartPage.addTshirtToCart();
+
+  // Checkout
+  await cartPage.clickCheckout();
+
+  // Validation
+  await expect(page).toHaveURL(/checkout/);
+});
