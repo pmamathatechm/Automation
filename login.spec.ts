0f4a59b1472dcfd580ad2b4c300a8cc904ccaf60
pmamathatechm <pmamatha160@gmail.com>
2026-05-13 21:36:16 +0530

    Update login.spec.ts

--- login.spec.ts
+++ login.spec.ts
@@ -1,10 +1,8 @@
 import { test, expect } from '@playwright/test';
-
 test('Login and Forgot Password', async ({ page }) => {
 
   // Open application
   await page.goto('https://automationteststore.com/index.php?rt=account/login');
-
   // Wait for page load
   await page.waitForLoadState('networkidle');
 
