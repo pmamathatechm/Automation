85c9dc40293b21083653902b19d1e3759b6041ca
pmamathatechm <pmamatha160@gmail.com>
2026-05-13 20:59:38 +0530

    Update login.spec.ts

---
+++ login.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('Login and Forgot Password', async ({ page }) => {
+
+  // Open application
+  await page.goto('https://automationteststore.com/index.php?rt=account/login');
+
+  // Wait for page load
+  await page.waitForLoadState('networkidle');
+
+  // ---------------- LOGIN ----------------
+
+  // Enter username
+  await page.locator('#loginFrm_loginname').fill('testuser');
+
+  // Enter password
+  await page.locator('#loginFrm_password').fill('Password123');
+
+  // Click Login button
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Validation after login
+  await expect(page).toHaveURL(/account/);
+
+});
